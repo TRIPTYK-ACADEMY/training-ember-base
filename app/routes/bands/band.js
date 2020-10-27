bf3ffa781d5610478b8a4c17318cf3afd0c45356
remadex <s.cardon@outlook.com>
2020-10-27 08:57:16 +0100

    subrouting

---
+++ app/routes/bands/band.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class BandsBandRoute extends Route {
+  model(params){
+    return this.store.findRecord('band', params.band_id, { include: 'songs'});
+  }
+}
