14bb14c995e5dac19f06244bfbcd29e10da61f85
remadex <s.cardon@outlook.com>
2020-10-30 10:52:16 +0100

    finish exo

--- app/components/detail-band.js
+++ app/components/detail-band.js
@@ -1,4 +1,18 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class DetailBandComponent extends Component {
+  @tracked isEditing = false;
+
+  @action
+  isEditingBandDetail(){
+    this.isEditing = true;
+  }
+
+  @action
+  editBandDetail(){
+    this.args.band.save();
+    this.isEditing = false;
+  }
 }
